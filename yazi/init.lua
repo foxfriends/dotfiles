988bfaae750de3c49063d6006ab9fa8576b2fa70
Cameron Eldridge <cameldridge@gmail.com>
2025-04-26 20:02:23 -0400

    use default git plugin config

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,10 +1,3 @@
-th.git = th.git or {}
-th.git.modified = ui.Style():fg("yellow")
-th.git.deleted = ui.Style():fg("red")
-th.git.added = ui.Style():fg("green")
-th.git.ignored = ui.Style():fg("white")
-th.git.untracked = ui.Style():fg("blue")
-
 require("git"):setup()
 
 require("full-border"):setup {
