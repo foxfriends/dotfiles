234a1c00ec3a74b4a02bad8dc3ecbed79dde90ea
Cameron Eldridge <cameldridge@gmail.com>
2025-08-19 16:04:02 -0400

    show each remote only once at most

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -23,19 +23,25 @@ function fish_prompt --description 'Write out the prompt'
 
   if git rev-parse --is-inside-work-tree > /dev/null 2>&1
     printf ' %s' (set_color $fish_color_vcs)
-    
+
+    set remote_github ""
+    set remote_rad ""
+    set remote_tangled ""
     for remote in (git remote)
       set remote_url (git remote show $remote -n | rg 'Fetch URL: (.*)' -or '$1')
       if echo "$remote_url" | rg 'github.com' -q
-        printf ' '
+        set remote_github ' '
       else if echo "$remote_url" | rg 'rad://' -q
-        printf '󰯉 '
+        set remote_rad '󰯉 '
       else if echo "$remote_url" | rg 'knot.eldridge.cam' -q
-        printf ' ' # TODO: tangled.sh doesn't have its own icon, so this is typst because it's similar
+        set remote_tangled ' ' # TODO: tangled.sh doesn't have its own icon, so this is typst because it's similar
       end
     end
 
-    printf '  %s' \
+    printf '%s%s%s %s' \
+      $remote_github \
+      $remote_rad \
+      $remote_tangled \
       (git branch | grep \* | cut -d ' ' -f2- | sed 's/^foxfriends/🦊/')
   end
 
